6e5867a850cebf821ee40387cd1239155c30ab74
Hostrider <rumyancev-2001@mail.ru>
2022-11-13 18:25:40 +0300

    second commit

--- lib/bloc/dude/dude_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'dude_bloc.dart';
-
-@immutable
-abstract class DudeEvent {}
--- lib/bloc/dude/dude_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'dude_bloc.dart';
-
-@immutable
-abstract class DudeState {}
-
-class DudeInitial extends DudeState {}
