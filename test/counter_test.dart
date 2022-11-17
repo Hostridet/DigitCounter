72962c52ae15b651e6b56cf265f8b6750381b0d1
nrumiantcev <rumiantcev.no@students.dvfu.ru>
2022-11-17 15:06:05 +1000

    added test

---
+++ test/counter_test.dart
@@ -0,0 +1,46 @@
+import 'package:dz_bloc/bloc/CounterBloc/counter_bloc.dart';
+import 'package:test/test.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+class MockCounterBloc extends MockBloc<CounterEvent, int> implements CounterBloc {}
+
+void main()
+{
+  mainBloc();
+}
+
+void  mainBloc()
+{
+  group("Counter", () {
+    blocTest<CounterBloc, int>(
+        'Nothing changed',
+        build: () => CounterBloc(),
+        expect: () => const <int>[],
+    );
+
+    blocTest<CounterBloc, int>(
+        "one increment",
+        build: () => CounterBloc(),
+        act: (bloc) => bloc.add(CounterIncrement()),
+        expect: () => const <int>[1],
+    );
+
+    blocTest<CounterBloc, int>(
+      "one decrement",
+      build: () => CounterBloc(),
+      act: (bloc) => bloc.add(CounterDecrement()),
+      expect: () => const <int>[-1],
+    );
+
+    blocTest<CounterBloc, int>(
+      "Clear",
+      build: () => CounterBloc(),
+      act: (bloc) {
+        bloc.add(CounterIncrement());
+        bloc.add(CounterNull());
+      },
+      expect: () => const <int>[1, 0],
+    );
+  } );
+}
+
